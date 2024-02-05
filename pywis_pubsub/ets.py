22e0f90bb84b752a4f9f7e970369f276811a1819
Tom Kralidis <tomkralidis@gmail.com>
2024-02-04 20:32:40 -0500

    update WNM namespace

--- pywis_pubsub/ets.py
+++ pywis_pubsub/ets.py
@@ -44,7 +44,7 @@ def gen_test_id(test_id: str) -> str:
     :returns: test identifier as URI
     """
 
-    return f'http://www.wmo.int/spec/wnm/1/req/conf/core/{test_id}'
+    return f'http://wis.wmo.int/spec/wnm/1/req/conf/core/{test_id}'
 
 
 class WNMTestSuite:
